a238322bf6b20a06cc6494621bcd9a75c8a62459
vCarpan <penapancarlos@gmail.com>
2023-12-13 14:23:28 +0100

    Cambios de drive

--- vmanager/src/App.vue
+++ vmanager/src/App.vue
@@ -157,9 +157,9 @@ const switchGroups = (vmId) => {
         </ul>        
           <div class="nav-item ms-auto">
             <div class="btn-group">
-              <button id="save" class="btn btn-outline-secondary" title="save">💾</button>
-              <button id="clean" class="btn btn-outline-secondary" title="clean">🧽</button>
-              <button id="restore" class="btn btn-outline-secondary" title="restore">↩️</button>
+              <button id="save" class="btn btn-outline-secondary" title="Guardar">💾 </button>
+              <button id="clean" class="btn btn-outline-secondary" title="Limpiar">🧽</button>
+              <button id="restore" class="btn btn-outline-secondary" title="Deshacer">↩️</button>
             </div>
           </div>
       </div>
